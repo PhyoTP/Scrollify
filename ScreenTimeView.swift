3bd0e28c8e152b6c1905ee71684cad09cd82c267
PhyoTP <phyo.thet.pai@sji.edu.sg>
2026-01-28 18:05:45 +0800

    added going back but im going to add new storylines. added tasks and more random feed generation

---
+++ ScreenTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  Scrollify
+//
+//  Created by Phyo Thet Pai on 24/1/26.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
